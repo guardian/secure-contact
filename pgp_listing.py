79307330da27999be82201e4bfb8c048887857bf
Kate Whalen <whalenka88@gmail.com>
2019-06-08 19:34:49 +0100

    Include fingerprints directly on the page

--- pgp_listing.py
+++ pgp_listing.py
@@ -29,14 +29,14 @@ def render_page(groups):
 
 groups = [
     Group('B', [
-        Entry('David Blishen', 'blishen pk', 'blishen fp'),
-        Entry('Michael Barton', 'barton pk', 'barton fp')
+        Entry('David Blishen', 'blishen pk', 'A820 60D2 AA7F E79C 98B7  43A1 A287 2CF2 263E 3238'),
+        Entry('Michael Barton', 'barton pk', '578E D4D8 23DE 6042 D189  A0DB 6AC4 9BE0 7B37 BC8C')
     ]),
     Group('C', [
-        Entry('Sam Cutler', 'cutler pk', 'cutler fp')
+        Entry('Sam Cutler', 'cutler pk', '5A83 E099 6D3A B407 52C1  CF5F B553 86B7 867B A22B')
     ]),
     Group('W', [
-        Entry('Kate Whalen', 'whalen pk', 'whalen fp')
+        Entry('Kate Whalen', 'whalen pk', '6FD2 E4C9 71AD B9BB 1573  85EA 383B C341 85FB BD09')
     ])
 ]
 
