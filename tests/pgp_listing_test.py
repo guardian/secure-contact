2fd46c96f454006e6342e223792ef0a936c89214
Kate Whalen <whalenka88@gmail.com>
2019-07-29 15:11:21 +0100

    better method of parsing email to catch edge case, along with test

--- tests/pgp_listing_test.py
+++ tests/pgp_listing_test.py
@@ -5,23 +5,36 @@ from pgp_listing import *
 
 class TestSortingEntries(unittest.TestCase):
     def setUp(self) -> None:
-        self.raw_fingerprint = """
+        self.fingerprint = """
         pub   4096R/85FBBD09 2019-03-11\n
         Key fingerprint = 6FD2 E4C9 71AD B9BB 1573  85EA 383B C341 85FB BD09\n
         uid       [ unknown] Kate Whalen <Kate.Whalen@theguardian.com>\n
         sub 2048R/8FA007E8 2019-03-11"""
 
+        self.fingerprint_without_angle_brackets = """
+        pub   4096R/85FBBD09 2019-03-11\n
+        Key fingerprint = 6FD2 E4C9 71AD B9BB 1573  85EA 383B C341 85FB BD09\n
+        uid       [ unknown] Kate.Whalen@theguardian.com\n\n"""
+
+    def test_parse_fingerprint(self):
+        expected = parse_fingerprint(self.fingerprint)
+        self.assertEqual('6FD2 E4C9 71AD B9BB 1573  85EA 383B C341 85FB BD09', expected)
+
     def test_parse_email(self):
-        expected = parse_email(self.raw_fingerprint)
+        expected = parse_email(self.fingerprint)
+        self.assertEqual('Kate.Whalen@theguardian.com', expected)
+
+    def test_parse_email_without_angle_brackets(self):
+        expected = parse_email(self.fingerprint_without_angle_brackets)
+        self.assertEqual('Kate.Whalen@theguardian.com', expected)
+
+    def test_obscure_email(self):
+        expected = obscure_email('Kate.Whalen@theguardian.com')
         self.assertEqual('Kate.Whalen<span class="leira">leira</span>[@]<span '
                          'class="leira">illusion</span>theguardian.com', expected)
 
-    def test_parse_fingerprint(self):
-        expected = parse_fingerprint(self.raw_fingerprint)
-        self.assertEqual('6FD2 E4C9 71AD B9BB 1573  85EA 383B C341 85FB BD09', expected)
-
     def test_enhance_entry(self):
-        result = enhance_entry(Entry('Kate Whalen', 'whalen pk', self.raw_fingerprint))
+        result = enhance_entry(Entry('Kate Whalen', 'whalen pk', self.fingerprint))
 
         self.assertEqual(result.other_names, 'Kate')
         self.assertEqual(result.last_name, 'Whalen')
